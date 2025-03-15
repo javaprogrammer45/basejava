009f65c8d5ea82b1e611cf72b75ed2e50ddd024e
Andrey <natureandpeople194@gmail.com>
2025-03-15 14:21:21 +0300

    +

--- src/com/basejava/storage/AbstractArrayStorage.java
+++ src/com/basejava/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package com.basejava.storage;
 
 import com.basejava.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -12,17 +14,35 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
-    public int size() {
-        return size;
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
     }
 
     public Resume get(String uuid) {
         int index = getIndex(uuid);
-        if (index == -1) {
+        if (index < 0) {
             System.out.println("Resume " + uuid + " not exist");
             return null;
         }
         return storage[index];
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
     }
 
     protected abstract int getIndex(String uuid);
--- src/com/basejava/storage/SortedArrayStorage.java
+++ src/com/basejava/storage/SortedArrayStorage.java
@@ -4,30 +4,39 @@ import com.basejava.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void update(Resume r) {
-
-    }
+public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void save(Resume r) {
-
+        if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (Integer.parseInt(r.getUuid().substring(5)) <
+                        Integer.parseInt(storage[i].getUuid().substring(5))) {
+                    System.arraycopy(storage, i, storage, i + 1, storage.length - 1);
+                    storage[i] = r;
+                } else {
+                    storage[size] = r;
+                    size++;
+                }
+            }
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            System.arraycopy(storage, index + 1, storage, index, storage.length - 1);
+            storage[index] = storage[index + 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     @Override
