4ed76d906d29144b3d48d25719ab83d9e395faa9
Andrey <natureandpeople194@gmail.com>
2025-03-21 14:03:20 +0300

    +

--- src/com/basejava/MainReflection.java
+++ src/com/basejava/MainReflection.java
@@ -3,9 +3,11 @@ package com.basejava;
 import com.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -13,6 +15,9 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        Method m = r.getClass().getDeclaredMethods()[3];
+        m.setAccessible(true);
+        m.invoke(r);
+        System.out.println(r.getUuid());
     }
 }
